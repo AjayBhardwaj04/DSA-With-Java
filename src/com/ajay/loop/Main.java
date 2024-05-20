7dfe5244b6302c64011f0ced8bc22e87a7963f41
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-05-20 23:16:39 +0530

    java loops codes

--- src/com/ajay/loop/Main.java
+++ src/com/ajay/loop/Main.java
@@ -1,4 +1,26 @@
 package com.ajay.loop;
 
 public class Main {
+    public static void main(String[] args) {
+
+
+        // This for loop will print the numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+// This while loop will print the numbers from 1 to 10, but it will stop if the user enters a negative number
+        int i = 1;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+
+// This do-while loop will print the numbers from 1 to 10, but it will always print the number 1, even if the user enters a negative number
+        int j = 1;
+        do {
+            System.out.println(i);
+            i--;
+        } while (i > 0);
+    }
 }
