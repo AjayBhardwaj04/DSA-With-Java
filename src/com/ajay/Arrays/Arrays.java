6fbced39c14196c6b0ff221c28a41dca0db16789
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-06-01 05:26:59 +0530

    java arrays func. code

--- src/com/ajay/Arrays/Arrays.java
+++ src/com/ajay/Arrays/Arrays.java
@@ -1,22 +1,9 @@
 package com.ajay.Arrays;
-
+import java.io.*;
 public class Arrays {
 
-        static void min(int arr[]){
-            int min=arr[0];
-            for(int i=1;i<arr.length;i++)
-                if(min>arr[i])
-                    min=arr[i];
 
-            System.out.println(min);
-        }
-
-        public static void main(String args[]){
-
-            int a[]={33,3,4,5};
-            min(a);//passing array to method
-
-        }}
+}
 
 
 
