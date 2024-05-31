e7783533ebc876c0cffb647f4a44a7491fc9e147
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-06-01 05:28:42 +0530

    java code

--- src/com/ajay/Arrays/Arrays02.java
+++ src/com/ajay/Arrays/Arrays02.java
@@ -1,10 +1,38 @@
 package com.ajay.Arrays;
 
 public class Arrays02 {
-    public static void main(String[] args) {
-        int getSum(int arr[],int n){
-            
+
+
+
+    static boolean checkPalindrome(int n)
+    {
+        int reverse = 0;
+        int temp = n;
+        while (temp != 0) {
+            reverse = (reverse * 10) + (temp % 10);
+            temp = temp / 10;
         }
+        return (reverse == n); // if it is true then it will return 1;
+        // else if false it will return 0;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        int n = 7007;
+        if (checkPalindrome(n) == true) {
+            System.out.println("Yes"+ n);
+        }
+        else {
+            System.out.println("No");
+        }
+
+
+
 
     }
 
