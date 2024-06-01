0f6e8d2f48eec9f79a0988fd65897842727ef095
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-06-01 20:11:49 +0530

    java Mathe func.code

---
+++ src/Mathes/countNum.java
@@ -0,0 +1,15 @@
+package Mathes;
+
+public class countNum {
+    static int countNums(int n){
+        if (n/10 == 0)
+            return n;
+
+        return 1 + countNums(n/10);
+    }
+
+    public static void main(String[] args) {
+        int n = 123234321;
+        System.out.println(countNums(n));
+    }
+}
