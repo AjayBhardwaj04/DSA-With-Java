1f9e9056c13dd3d2daf5818f3972dcc6105600ed
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-06-01 20:12:44 +0530

    java codes

--- src/com/ajay/Arrays/Arrays.java
+++ src/com/ajay/Arrays/Arrays.java
@@ -1,7 +1,20 @@
 package com.ajay.Arrays;
-import java.io.*;
 public class Arrays {
+    static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
 
+        return n * factorial(n - 1);
+    }
+
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num
+                + " is " + factorial(5));
+        System.out.println("Ajay");
+    }
 
 }
 
