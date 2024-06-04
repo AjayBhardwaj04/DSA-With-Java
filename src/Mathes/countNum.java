bf14f8bc7f9848c9177b96a2ec395a2f7396c5f1
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-06-05 01:45:30 +0530

    Mathe count num func. codes

--- src/Mathes/countNum.java
+++ src/Mathes/countNum.java
@@ -1,15 +1,30 @@
 package Mathes;
 
 public class countNum {
-    static int countNums(int n){
-        if (n/10 == 0)
+//    static int countNums(int n){
+//        if (n/10 == 0)
+//            return n;
+//
+//        return 1 + countNums(n/10);
+//    }
+//
+//    public static void main(String[] args) {
+//        int n = 12321;
+//        System.out.println(countNums(n));
+//    }
+
+    static int singleDigit(int n){
+        // code here
+
+        if(n/10 == 0)
             return n;
+        return 1 + singleDigit(n / n);
 
-        return 1 + countNums(n/10);
     }
 
-    public static void main(String[] args) {
-        int n = 12321;
-        System.out.println(countNums(n));
+    public static void main(String[] args){
+        int n = 123;
+        System.out.println(singleDigit(n));
     }
+
 }
--- src/Mathes/factorial.java
+++ src/Mathes/factorial.java
@@ -1,2 +1,46 @@
-package Mathes;public class factorial {
+package Mathes;
+
+public class factorial {
+    static int factorial(int n)
+    {
+//        int res = 1, i;
+//        for (i = 1; i <= n; i++)
+//            res = res * i;
+//        return res;
+//        int j = 1 ,i ;
+//        for (i = 1; i <= n ; i++)
+//            j = j * i;
+//        return j;
+//    }
+
+//
+//        if (n < 0) {
+//            return -1;
+//        } else if (n == 0) {
+//            return 1;
+//        } else {
+//            long result = 1;
+//            for (int i = 2; i <= n; i++) {
+//                result = result * i;
+//                // Check for overflow
+//                if (result < 0) {
+//                    throw new ArithmeticException("Factorial overflows a long value for number: " + n);
+//                }
+//            }
+//            return result;
+
+        if (n==0)
+        return 1;
+        return n * factorial(n-1);
+
+
+        }
+
+    public static void main(String[] args) {
+int num = 6;
+        System.out.println(factorial(num));
+
+    }
+
+
 }
