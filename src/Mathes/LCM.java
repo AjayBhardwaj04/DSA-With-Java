df7d841d7e5bbbb763e0de27f4c5ed39a8eda3f2
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-06-08 16:41:48 +0530

    counte Lcm numbers

--- src/Mathes/LCM.java
+++ src/Mathes/LCM.java
@@ -1,21 +1,41 @@
 package Mathes;
 
+import java.util.Scanner;
+
 public class LCM {
-    static int lcm(int a , int b)
+    static int gcd(int a , int b)
     {
-//        res = Math.max(a,b);
-        int res = Math.max(a,b);
-        while (true)
-        {
-            if (res % a == 0 && res % b == 0)
-                return res;
-            res++;
-        }
+////        First Methode !!!
+////        res = Math.max(a,b);
+//        int res = Math.max(a,b);
+//        while (true)
+//        {
+//            if (res % a == 0 && res % b == 0)
+//                return res;
+//            res++;
+//        }
+
+        //    Second Methode !!!!
+
+        if (b == 0)
+            return a;
+        return gcd(b,a % b);
+
+    }
+    static int lcm (int a , int b )
+    {
+        return (a * b) / gcd(a ,b);
     }
 
+
+
+
+    
+
     public static void main(String[] args) {
+//        Scanner Scenner = new Scanner(System.in);
         int a = 10;
-        int b = 10;
+        int b = 12;
         System.out.println(lcm(a , b));
     }
 }
