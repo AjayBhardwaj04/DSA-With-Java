d172c8c490a5477dd93bf47245f4286f9c38ed96
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-06-11 14:31:12 +0530

    calculate the Prime Factors

---
+++ src/Mathes/primeFactors.java
@@ -0,0 +1,21 @@
+package Mathes;
+
+public class primeFactors {
+    static void primeFact(int n){
+        if (n <= 1) return;
+        for (int i = 2; i*i < n; i++){
+            while (n%i == 0){
+                System.out.println(i);
+                n = n/i;
+            }
+        }
+if (n > 1)
+    System.out.println(n);
+        }
+
+    public static void main(String[] args) {
+        int n = 80;
+        primeFact(n);
+    }
+
+}
