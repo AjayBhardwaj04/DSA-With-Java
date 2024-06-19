8cf9df6ccf5e658777196f4e6e02c28df31a2e2d
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-06-19 11:07:34 +0530

    Prime Factors 2

---
+++ src/Mathes/primeFactors2.java
@@ -0,0 +1,41 @@
+package Mathes;
+
+public class primeFactors2 {
+    static  void  primeFactor2(int i){
+        if (i <= 1)
+            return;
+        while (n % 2 == 0 )
+        {
+            System.out.println(2 +" ");
+            n /= 2;
+        }
+        while (n % 3 == 0)
+        {
+            System.out.println(3 +" ");
+            n /= 3;
+        }
+        for (int = 5 ; i*i <= n; i = i +6 )
+        {
+            while (n % i == 0)
+            {
+                System.out.println(i+"  ");
+                n /=i;
+            }
+            while (n % (i+2))
+            {
+                n /= (2+n);
+            }
+
+
+        }
+        if (n > 3)
+            System.out.println(n + "  ");
+        System.out.println();
+
+            }
+
+    public static void main(String[] args) {
+int n = 325;
+primeFactors2(n;
+    }
+}
