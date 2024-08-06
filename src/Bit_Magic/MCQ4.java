0b47c38b0ab6567d5d1dbfb4684a6701d290389e
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-08-06 20:35:55 +0530

    bit MCQ que. code

--- src/Bit_Magic/MCQ4.java
+++ src/Bit_Magic/MCQ4.java
@@ -3,9 +3,22 @@ package Bit_Magic;
 public class MCQ4 {
 
 
+    public static int countBitsToFlip(int a, int b) {
+        int xor = a ^ b;
+        int count = 0;
+
+        // Efficiently count set bits using Brian Kernighan's algorithm
+        while (xor != 0) {
+            xor &= (xor - 1);
+            count++;
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
-        int n = 5;
-        int x = 3;
-        System.out.println(n^x);
+        int a = 11, b = 9;
+        int flips = countBitsToFlip(a, b);
+        System.out.println("Number of bits to be flipped: " + flips);
     }
 }
---
+++ src/Bit_Magic/que2.java
@@ -0,0 +1,2 @@
+package Bit_Magic;public class que2 {
+}
