4fa3f7432d0c87bc286143c46042a19d03d893dd
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-08-06 20:37:08 +0530

    Bit Que. prac. code

--- src/Bit_Magic/que2.java
+++ src/Bit_Magic/que2.java
@@ -1,2 +1,26 @@
-package Bit_Magic;public class que2 {
+package Bit_Magic;
+
+public class que2 {
+    public static int getFirstSetBit(int n) {
+        int position = 1;
+
+        // Check if n is negative or zero
+        if (n <= 0) {
+            return 0;
+        }
+
+        // Find the first set bit
+        while ((n & 1) == 0) {
+            n >>= 1;
+            position++;
+        }
+
+        return position;
+    }
+
+    public static void main(String[] args) {
+        int n = 18;
+        int result = getFirstSetBit(n);
+        System.out.println("Position of first set bit: " + result);
+    }
 }
