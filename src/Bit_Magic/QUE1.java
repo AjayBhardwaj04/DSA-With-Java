909a13905b2674bf593c9f1305004e99582d6f6d
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-08-07 19:57:57 +0530

    Bit Magic Que. 1 sou. code

--- src/Bit_Magic/QUE1.java
+++ src/Bit_Magic/QUE1.java
@@ -1,2 +1,23 @@
-package Bit_Magic;public class QUE1 {
+package Bit_Magic;
+
+public class QUE1 {
+    public static boolean isKthBitSet(int n, int k) {
+        // Create a mask with only the kth bit set
+        int mask = 1 << k;
+
+        // Check if the kth bit is set by performing bitwise AND
+        return (n & mask) != 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 4; // Binary: 1010
+        int k = 1;
+
+        if (isKthBitSet(n, k)) {
+            System.out.println("The " + k + "th bit is set");
+        } else {
+            System.out.println("The " + k + "th bit is not set");
+        }
+    }
+
 }
