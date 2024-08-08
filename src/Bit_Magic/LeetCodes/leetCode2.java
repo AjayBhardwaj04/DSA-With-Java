8ffa3586282e6818bbb0317fa366baa412d906e8
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-08-08 21:26:07 +0530

    bit Mainpulation leetCode ques.

--- src/Bit_Magic/LeetCodes/leetCode2.java
+++ src/Bit_Magic/LeetCodes/leetCode2.java
@@ -1,2 +1,16 @@
-package Bit_Magic.LeetCodes;public class leetCode2 {
+package Bit_Magic.LeetCodes;
+
+public class leetCode2 {
+    public  static int findeNum(int[] nums){
+        int ans = 0;
+        for (int num : nums){
+            ans = ans ^ num;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] ans = {12,12,1,2,13,2,2,3,};
+        System.out.println(findeNum(ans));
+    }
 }
