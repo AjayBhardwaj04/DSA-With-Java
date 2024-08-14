10397f2d95415d144f6a4d7fe6933ff3ca4cdc0b
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-08-14 21:12:00 +0530

    COUNT THE POWER OF NUMBERS :)

---
+++ src/Bit_Magic/LeetCodes/powerTow.java
@@ -0,0 +1,18 @@
+package Bit_Magic.LeetCodes;
+// COUNT THE POWER OF NUMBERS :)
+public class powerTow {
+    public static boolean isPowerOfTwo(int n) {
+        for (int i = 0; i < 1; i++) {
+            int ans = (int) Math.pow(2, i);
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int i = 101;
+        System.out.println(isPowerOfTwo(i));
+    }
+}
