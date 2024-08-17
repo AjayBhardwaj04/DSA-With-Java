bd6025abd8716f1c561458bd17d99d5d32ebd73d
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-08-17 20:17:10 +0530

    java code

--- src/Bit_Magic/QUE1.java
+++ src/Bit_Magic/QUE1.java
@@ -20,4 +20,37 @@ public class QUE1 {
         }
     }
 
+    public static class QUN3 {
+        public static int countAllBits(int n){
+            //Ignoring 0 as all the bits are unset.
+            n+=1;
+            int count=0;
+
+            //Counting set bits from 1 to n.
+            for( int x=2 ; x/2<n ; x=x*2 )
+            {
+                //Total count of pairs of 0s and 1s.
+                int quotient = n/x;
+                //quotient gives the complete count of pairs of 1s.
+                //Multiplying it with the (current power of 2)/2 will give
+                //the count of 1s in the current bit.
+                count += quotient * x / 2;
+
+                int remainder = n%x;
+                //If the count of pairs is odd then we add the remaining 1s
+                //which could not be grouped together.
+                if(remainder > x/2)
+                    count += remainder - x/2;
+            }
+
+            //returning count of set bits.
+            return count;
+
+        }
+
+        public static void main(String[] args) {
+            int num = 7;
+            System.out.println(countAllBits(num));
+        }
+    }
 }
