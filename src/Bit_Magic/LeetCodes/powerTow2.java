f1f7fd869c32d78359119326cb005d1fa47f3148
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-08-26 20:30:34 +0530

    power of tow

--- src/Bit_Magic/LeetCodes/powerTow2.java
+++ src/Bit_Magic/LeetCodes/powerTow2.java
@@ -1,15 +1,25 @@
 package Bit_Magic.LeetCodes;
 
 public class powerTow2 {
-    public static boolean powerOfTow(int n){
-        if (n == 0){
+    public static boolean powerOfTow(double n) {
+//        if (n == 0) {
+//            return false;
+//        }
+//        return (n != 0) && ((n & (n - 2)) == 0);
+//    }
+
+        if(n <= 0){
             return false;
         }
-        return (n != 0) && ((n & (n - 2)) == 0);
+        while (n % 2 == 0){
+            n = n /  2;
+        }
+        return n == 1;
     }
 
     public static void main(String[] args) {
-        int num = 8;
+        double num = 4.50;
         System.out.println(powerOfTow(num));
+
     }
 }
