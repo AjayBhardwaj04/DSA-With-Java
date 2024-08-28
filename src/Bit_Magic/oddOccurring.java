619737994d42b9575f32303c68d9809667d40739
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-08-28 19:56:45 +0530

    Odd Occures numbers count code update

--- src/Bit_Magic/oddOccurring.java
+++ src/Bit_Magic/oddOccurring.java
@@ -3,15 +3,19 @@ package Bit_Magic;
 public class oddOccurring {
      static int OneOddOccurring(int arr[], int arr_size) {
         int i;
-        int res = 0;
-        for (i = 0; i < arr_size; i++) {
+        int res = arr[0];
+        for ( i = 0; i < arr_size; i++) {
             res = res ^ arr[i];
+
+            for (int j = 0; j < arr_size; j++){
+                res = res ^ arr[j];
+            }
         }
-        return res % 2;
+        return res ;
     }
 
     public static void main(String[] args) {
-        int arr[] = new int[]{2,2,2,2,2,2,5};
+        int arr[] = new int[]{2,3,3,3};
         int n = arr.length;
         System.out.println(OneOddOccurring(arr ,n));
     }
