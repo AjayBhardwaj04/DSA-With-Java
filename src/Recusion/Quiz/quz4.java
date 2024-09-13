159ff1e5264f5120349c2aa002b5ba5543aa0873
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-13 21:12:27 +0530

    Quiz 4 Solution

--- src/Recusion/Quiz/quz4.java
+++ src/Recusion/Quiz/quz4.java
@@ -1,2 +1,16 @@
-package Recusion.Quiz;public class quz4 {
+package Recusion.Quiz;
+
+public class quz4 {
+    static int quz(int n){
+        if (n ==0){
+            return 1 ;}
+        System.out.println(n%2);
+        return quz(n/2);
+
+    }
+
+    public static void main(String[] args) {
+        int n = 25;
+        System.out.println(quz(n));
+    }
 }
