16f94267d2a7c272ff8ac67bdf39ba9ef1c34764
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-13 21:10:55 +0530

    Quiz 2 Solution

---
+++ src/Recusion/Quiz/quiz2.java
@@ -0,0 +1,16 @@
+package Recusion.Quiz;
+
+public class quiz2 {
+     static int res2(int n){
+        if (n == 4)
+            return n;
+
+            else return 2*res2(n+1);
+
+    }
+
+    public static void main(String[] args) {
+         int a = 2;
+        System.out.println(res2(a));
+    }
+}
---
+++ src/Recusion/Quiz/quz4.java
@@ -0,0 +1,2 @@
+package Recusion.Quiz;public class quz4 {
+}
