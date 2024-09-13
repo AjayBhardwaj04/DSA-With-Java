9000eb1d036e15a0e158e47cbf1632efad66e347
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-13 21:11:49 +0530

    Quiz 3 Solution

--- src/Recusion/Quiz/quiz3.java
+++ src/Recusion/Quiz/quiz3.java
@@ -1,2 +1,18 @@
-package Recusion.Quiz;public class quiz3 {
+package Recusion.Quiz;
+
+public class quiz3 {
+    static int  quz(int a , int b){
+        if (a == 0){
+            return b;
+        }
+        return quiz3.quz(a - 1, a + b);
+    }
+
+    public static void main(String[] args) {
+        int a = 4; // Replace with your desired initial value for a
+        int b = 3; // Replace with your desired initial value for b
+        int result = quiz3.quz(a,b);
+        System.out.println("Result: " + result);
+//        System.out.println(quiz3(a,));
+    }
 }
