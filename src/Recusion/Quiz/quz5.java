bdd103195604e1c7708797643a8eb5b37f0c8114
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-13 21:13:11 +0530

    Quiz 5 Solution

--- src/Recusion/Quiz/quz5.java
+++ src/Recusion/Quiz/quz5.java
@@ -1,2 +1,17 @@
-package Recusion.Quiz;public class quz5 {
+package Recusion.Quiz;
+//Quiz 5 is not solve
+public class quz5 {
+    static int fun(int x, int y) {
+        if (x == 0) {
+            return 0;
+        }
+
+        return x + fun(x, y - 1);
+    }
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+        System.out.println(fun(x,y));
+    }
 }
