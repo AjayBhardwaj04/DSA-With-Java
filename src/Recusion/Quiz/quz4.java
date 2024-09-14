f7fba8a0dc23bf32c2de7eff350e1cc19a141172
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-14 20:06:02 +0530

    code modify :

--- src/Recusion/Quiz/quz4.java
+++ src/Recusion/Quiz/quz4.java
@@ -2,15 +2,15 @@ package Recusion.Quiz;
 
 public class quz4 {
     static int quz(int n){
-        if (n ==0){
-            return 1 ;}
-        System.out.println(n%2);
-        return quz(n/2);
+        if (n ==1){
+            return 0 ;}
+//        System.out.println(n%2);
+         else return 1 + quz(n/2);
 
     }
 
     public static void main(String[] args) {
-        int n = 25;
+        int n = 17;
         System.out.println(quz(n));
     }
 }
