491c17c1a1d241c8eacfce1bffcc3cc5f912a12e
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-17 20:57:07 +0530

    code modify :

--- src/Recusion/Quiz/quz5.java
+++ src/Recusion/Quiz/quz5.java
@@ -2,8 +2,8 @@ package Recusion.Quiz;
 //Quiz 5 is not solve
 public class quz5 {
     static int fun(int x, int y) {
-        if (x == 0) {
-            return 0;
+        if (x == 1) {
+            return 0 ;
         }
 
         return x + fun(x, y - 1);
