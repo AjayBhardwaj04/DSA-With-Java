9b1711ba6cd994226a2e989a774df4cca19d65e8
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-17 20:57:58 +0530

    count the power of nums

--- src/Recusion/priNumForRec.java
+++ src/Recusion/priNumForRec.java
@@ -1,2 +1,39 @@
-package Recusion;public class priNumForRec {
+package Recusion;
+
+public class priNumForRec {
+    public  double priNumForRec(double x, int n) {
+        return priNumForRec(x, (long) n);}
+
+
+    private double priNumForRec(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 1 / priNumForRec(x, -n);
+        }
+
+        if (n % 2 == 1) {
+            return x * priNumForRec(x * x, (n - 1) / 2);
+        } else {
+            return priNumForRec(x * x, n / 2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+//        myClass fun = new myClass();
+        priNumForRec fun = new priNumForRec();
+        System.out.println(fun.priNumForRec(2.0,4));
+//        fun.priNumForRec(2.0,4);
+
+//        double x = 2.1;
+//        int n = 2;
+//
+//        priNumForRec(x,n);
+
+//        System.out.println(priNumForRec(x, n));
+    }
+
 }
