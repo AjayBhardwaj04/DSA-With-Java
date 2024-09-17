a14a188d111849cf0709eb0b5aa25277ef667c8e
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-17 22:12:52 +0530

    print numbers

--- src/Recusion/printForNum.java
+++ src/Recusion/printForNum.java
@@ -1,2 +1,17 @@
-package Recusion;public class printForNum {
+package Recusion;
+
+public class printForNum {
+    static void  fun(int n){
+        if (n == 0){
+            return ;
+        }
+        System.out.print(n + " ");
+        fun(n -1);
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        fun(n);
+//        System.out.println(fun(n));
+    }
 }
