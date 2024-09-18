1d96d32002d32cb0a73b848319657c254ab7779e
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-18 20:40:44 +0530

    code UPdate

--- src/Recusion/printForNum.java
+++ src/Recusion/printForNum.java
@@ -5,13 +5,15 @@ public class printForNum {
         if (n == 0){
             return ;
         }
+        fun(n - 1);
         System.out.print(n + " ");
         fun(n -1);
     }
 
     public static void main(String[] args) {
-        int n = 12;
+        int n =4;
         fun(n);
 //        System.out.println(fun(n));
     }
+//    Time complexity is : T(n) = T(n-1) + cont(1);
 }
