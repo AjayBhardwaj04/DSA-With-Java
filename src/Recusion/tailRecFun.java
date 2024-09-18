bf95d5b3781ca11beda5d7456ecb1d1485570ccf
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-18 20:41:38 +0530

    Tail Recursion Function

--- src/Recusion/tailRecFun.java
+++ src/Recusion/tailRecFun.java
@@ -1,2 +1,19 @@
-package Recusion;public class tailRecFun {
+package Recusion;
+// Not Tail Recursion Function
+public class tailRecFun {
+    public static void tailFun(int n , int k){
+        if (n == 0){
+            return;
+        }                                                  // Base Cases
+        System.out.print(k + " ");
+        tailFun(n - 1, k + 1);
+
+    }
+
+    public static void main(String[] args) {
+        int n =  11;
+        int k = 1;
+        tailFun(n,k);
+//        System.out.println(tailFun(n,k));
+    }
 }
