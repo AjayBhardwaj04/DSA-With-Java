4121473f17bc741f22ed54bb37091cc92f398a3e
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-19 21:42:25 +0530

    Write a Base Case for Recursion :

--- src/Recusion/writeBaseCase.java
+++ src/Recusion/writeBaseCase.java
@@ -1,2 +1,24 @@
-package Recusion;public class writeBaseCase {
+package Recusion;
+
+public class writeBaseCase {
+    public static int writeCase(int n){
+        if (n == 0 ){
+            return 0 ;
+        }
+        if (n == 1){
+            return 1;
+        }
+        System.out.println(n + " ");
+         writeCase(n - 1 );
+        writeCase(n - 2 );
+
+    // + writeCase(n -2);
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+//        writeCase(n);
+        System.out.println(writeCase(n));
+    }
 }
