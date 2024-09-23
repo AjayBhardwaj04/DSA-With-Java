76bfdeaf4cfd0bcdae7898f458545730819a5789
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-23 20:31:39 +0530

    Problem No. 3

--- src/Recusion/Problem/qun3.java
+++ src/Recusion/Problem/qun3.java
@@ -1,2 +1,15 @@
-package Recusion.Problem;public class qun3 {
+package Recusion.Problem;
+
+public class qun3 {
+    static int isFibonacci(int n){
+        if (n <= 1){
+            return n;
+        }
+        return isFibonacci(n -1) + isFibonacci(n -2);
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println(isFibonacci(n));
+    }
 }
