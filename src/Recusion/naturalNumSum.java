7d2578374ced1d5a1e9137168297f2a88ff6428f
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-24 20:41:05 +0530

    code update

--- src/Recusion/naturalNumSum.java
+++ src/Recusion/naturalNumSum.java
@@ -1,15 +1,20 @@
 package Recusion;
 //Given a number n, find sum of first n natural numbers. To calculate the sum, we will use a recursive function recur_sum().
 public class naturalNumSum {
-    public static int naturalSum(int n) {
+     static  int naturalSum(int n) {
         if (n == 0){
-            return 0;
-        }
-        return n + naturalSum(n - 1);
+            return 0 ;
+        }else
+            return n + naturalSum(n-1);
+
+
+
+
     }
 
     public static void main(String[] args) {
         int n = 4;
+//        naturalSum(n);
         System.out.println(naturalSum(n));
     }
 }
