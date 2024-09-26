8a8cf253a039c176ddedf60bd508861a0408ec04
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-26 20:36:45 +0530

    Count power of subset in String

---
+++ src/Recusion/Problem/powerSet.java
@@ -0,0 +1,22 @@
+package Recusion.Problem;
+
+public class powerSet {
+    public static void  powerSetRecursion(String str, int index, String curr){
+
+        int n = str.length();
+        if (n == index){
+            System.out.println(curr);
+            return;
+        }
+        powerSetRecursion(str, index +1, curr + str.charAt(index));
+        powerSetRecursion(str,index + 1,curr);
+    }
+
+    public static void main(String[] args) {
+        String srt = "abc";
+        int index = 0;
+        String curr = " ";
+//        System.out.println(powerSetRecursion(srt,index,));
+        powerSetRecursion(srt,index,curr);
+    }
+}
