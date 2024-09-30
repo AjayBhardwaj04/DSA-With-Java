bd500c79b3d1db531b3791acf476a3eba0c7b91b
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-09-30 20:03:42 +0530

    Josephus Problem

---
+++ src/Recusion/josephusProb.java
@@ -0,0 +1,26 @@
+package Recusion;
+
+//A total of n people are standing in a circle, and you are one of them playing a game. Starting from a person,
+// k persons will be counted in order along the circle, and the kth person will be killed. Then the next k persons will be counted along the circle,
+// and again the kth person will be killed. This cycle will continue until only a single person is left in the circle.
+//If there are 5 people in the circle in the order A, B, C, D, and E, you will choose k=3.
+// Starting from A, you will count A, B and C. C will be the 3rd person and will be killed.
+// Then you will continue counting from D, E and then A. A will be third person and will be killed.
+//You will be given an array where the first element is the first person from whom the counting will start and the subsequent order of the people.
+// You want to be the last one standing. Determine the index at which you should stand to survive the game. Return an integer denoting safe position.
+public class josephusProb {
+    public static int josephus(int n, int k) {
+        if (n == 1) {
+            return 1;
+        } else
+            return (josephus(n - 1, k) + k - 1) % n + 1;
+
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int k = 3;
+        System.out.println(josephus(n, k));
+    }
+}
