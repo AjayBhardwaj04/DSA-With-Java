c2fbcfb2386d9a612a46edc62395de24f3535d24
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-10-02 00:42:41 +0530

    Palindrome problem Qun.

--- src/Recusion/palindrome.java
+++ src/Recusion/palindrome.java
@@ -4,13 +4,15 @@ public class palindrome {
       static boolean  isPalindrome(String str, int start, int end){
         if (start >= end) {
             return true;
+//            System.out.println();
         }
 
         return  (str.charAt(start)== str.charAt(end)) && isPalindrome(str,start + 1, end -1) ;
+
     }
 
     public static void main(String[] args) {
-        String str = "ABBA";
+        String str = "ABABA";
 
         int start = 0;
         int end  = str.length()-1;
