3d74d9ace2b04ca3a5f2af55a754e8308ffea518
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-10-03 02:45:37 +0530

    Subset of Sum Recursion Que.

--- src/Recusion/subsetSumProb.java
+++ src/Recusion/subsetSumProb.java
@@ -1,24 +1,27 @@
-package Recusion;
-//Subset Sum Problem (Recursive Solution) (Medium)
-public class subsetSumProb {
-    public static int subsetSum(int arr[], int num,int sum){
-        if (num == 0 || sum == 0){
-            return (sum == 0) ? 1 : 0;
-        }
-        if (arr[num - 1] > sum){
-            return subsetSum(arr,num-1,sum);
+package Recursion;
 
+public class subsetSumProb {
+    public static boolean subsetSum(int arr[], int num, int sum) {
+        if (sum == 0) {
+            return true;
         }
-        return subsetSum(arr, num-1,sum)+
-        subsetSum(arr,num-1,sum-arr[num-1]);
+        if (num == 0) {
+            return false;
+        }
+
+        if (arr[num - 1] > sum) {
+            return subsetSum(arr, num - 1, sum);
+        }
+
+        return subsetSum(arr, num - 1, sum) ||
+                subsetSum(arr, num - 1, sum - arr[num - 1]);
     }
 
     public static void main(String[] args) {
-        int arr[] = {10,15,20};
+        int arr[] = {10, 15, 20};
         int sum = 25;
         int num = arr.length;
 
-        System.out.println(subsetSum(arr,sum,num));
+        System.out.println(subsetSum(arr, num, sum));
     }
-
 }
