cc67ffef0353c0f4f87f76c89fb8e51054022cb0
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-10-16 21:03:25 +0530

    a

---
+++ src/Arrays/largestElmIArr.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class largestElmIArr {
+    static int getLargestNum(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {//checking all element in Array 
+            boolean flag = true;//re
+
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true) {
+                return arr[i];
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 23, 454, 345, 3};
+        System.out.println(getLargestNum(arr));
+    }
+}
