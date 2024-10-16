06f0685a749dcadb721b35ff64b7dba6273f1d81
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-10-16 21:05:33 +0530

    code Update

--- src/Recusion/writeBaseCase.java
+++ src/Recusion/writeBaseCase.java
@@ -3,7 +3,7 @@ package Recusion;
 public class writeBaseCase {
     public static int writeCase(int n){
         if (n == 0 ){
-            return -1 ;
+            return 1 ;
         }
 //        if (n == 1){
 //            return 0;
