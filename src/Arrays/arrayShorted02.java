c437f70b2161a1a11d43a3cee6873ed8f2696ac7
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2024-10-22 20:26:51 +0530

    Array Short in Array list

--- src/Arrays/arrayShorted02.java
+++ src/Arrays/arrayShorted02.java
@@ -1,2 +1,16 @@
-package Arrays;public class arrayShored02 {
+package Arrays;
+
+public class arrayShorted02 {
+    public static boolean arrShorted(int arr[]){
+        for (int i=1; i<arr.length;i++){
+            if (arr[i] < arr[i-1])
+                return false;
+        }
+        return arrShorted(arr);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        System.out.println(arrShorted(arr));
+    }
 }
